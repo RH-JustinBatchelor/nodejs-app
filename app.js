065f8673d1a68f4e0abc081d5d75ea924cee7eb8
Justin Batchelor <jbatchel@redhat.com>
2022-07-14 15:28:10 -0400

    Change server config

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express');
+var http = require('http');
 var app = express();
-
+var server = http.createServer(app);
 
 app.get('/', (req, res) => {
     res.send("Hello from Node.js Starter Application");
+});
+
+
+server.listen(3000, () => {
+    console.log(`App started on PORT 3000`);
 });
